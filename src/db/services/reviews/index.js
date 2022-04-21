14e6b496458bfd3cedebbb48b385dacb76b8b274
BertoPolo <bertopolo91@gmail.com>
2022-04-21 19:45:31 +0200

    Update index.js

--- src/db/services/reviews/index.js
+++ src/db/services/reviews/index.js
@@ -48,3 +48,33 @@ reviewsRouter.delete("/reviewId", async (req, res, next) => {
 })
 
 export default reviewsRouter
+
+/* Review
+The review should contain the following information:
+
+{
+	    "id": 1,
+	    "text": TEXT,
+	    "username:STRING,
+	    "productId":FOREIGN KEY products
+      
+}
+ 
+ 
+Routes
+GET /reviews => returns the list of reviews including product
+GET /reviews /123 => returns a single review with product
+POST /reviews => create a new review
+PUT /reviews /123 => edit the review with the given id
+DELETE /reviews /123 => delete the review with the given id
+ 
+
+Model Quering
+
+Implement search on products by  name, description
+Implement filters by price range
+order products by price in asc/desc order
+ 
+
+The persistence must be granted via Postgres
+ */
